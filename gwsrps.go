534bb61bbe2e5b723e69fa63c95acf755b155571
edoceo <code@edoceo.com>
2019-05-30 03:12:11 -0700

    add Redis to the mix

--- gwsrps.go
+++ gwsrps.go
@@ -14,33 +14,66 @@ import "path/filepath"
 import "sync"
 
 import "github.com/gorilla/websocket"
+import "github.com/go-redis/redis"
 import "github.com/oklog/ulid"
 
-type WS_Client_List struct {
-	client_list map[*WS_Client]bool
-}
+// type WS_Client_List struct {
+// 	client_list map[*WS_Client]int
+// }
 
 type WS_Client struct {
 	id string
 	ws *websocket.Conn
+	pub *redis.Client
+	sub *redis.PubSub
 	pump chan []byte
 }
-
-func generateULID() string {
-	t := time.Unix(1000000, 0)
-	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-	u, _ := ulid.New(ulid.Timestamp(t), entropy)
-	return u.String()
-	// Output: 0000XSNJG0MQJHBF4QX1EFD6Y3
-}
-
 
 /**
  * Read Message from WebSocket, Publish to Redis
  * @param {[type]} wsc [description]
  * @return {[type]} [description]
  */
-func (wsc *WS_Client) readPump() {
+func (c *WS_Client) incomingPump() {
+
+	defer func() {
+		// c.hub.unregister <- c
+		fmt.Println("incomingPump - defer")
+		c.ws.Close()
+		// c.pub.Close()
+		c.sub.Close()
+	}()
+
+	pongWait := 60 * time.Second
+
+	c.ws.SetReadLimit(32768)
+
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	for {
+		msgType, msgBody, err := c.ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				fmt.Printf("error: %v", err)
+			}
+			break
+		}
+
+		switch (msgType) {
+		case websocket.TextMessage:
+			fmt.Println("incoming-ws:", string(msgBody))
+			// fmt.Println("Text Message")
+			break;
+		case websocket.BinaryMessage:
+			fmt.Println("Data Message")
+			break;
+		}
+
+		// message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		// c.hub.broadcast <- message
+		c.pub.Publish("gwsrps", msgBody)
+	}
 
 }
 
@@ -49,29 +82,90 @@ func (wsc *WS_Client) readPump() {
  * @param {[type]} wsc [description]
  * @return {[type]} [description]
  */
-func (wsc *WS_Client) sendPump() {
+func (c *WS_Client) outgoingPump() {
+
+	sendWait := 8 * time.Second
+	pingWait := 60 * time.Second * 9 / 10
+
+	tick := time.NewTicker(pingWait)
+
+	defer func() {
+		fmt.Println("outgoingPump - defer")
+		// c.hub.unregister <- c
+		tick.Stop()
+		c.ws.Close()
+		//c.pub.Close()
+		c.sub.Close()
+	}()
+
+
+	for {
+		fmt.Println("Line 098")
+		select {
+		case msgBody, ok := <- c.pump:
+
+			fmt.Println("outgoing-gws:", string(msgBody))
+
+			c.ws.SetWriteDeadline(time.Now().Add(sendWait))
+
+			if !ok {
+				// The hub closed the channel.
+				c.ws.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.ws.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(msgBody)
+
+			// // Add queued chat messages to the current websocket message.
+			// n := len(c.pump)
+			// for i := 0; i < n; i++ {
+			// 	w.Write(newline)
+			// 	w.Write(<-c.pump)
+			// }
+
+			// Close Error, Kill It
+			err112 := w.Close()
+			if err112 != nil {
+				return
+			}
+
+			break
+
+		case <-tick.C:
+
+			c.ws.SetWriteDeadline(time.Now().Add(sendWait))
+			err127 := c.ws.WriteMessage(websocket.PingMessage, nil)
+			if err127 != nil {
+				return
+			}
+
+		}
+	}
 
 }
 
-/*
-var (
-    gStore      *Store
-    gPubSubConn *redis.PubSubConn
-    gRedisConn  = func() (redis.Conn, error) {
-        return redis.Dial("tcp", ":6379")
-    }
-)
-func init() {
-    gStore = &Store{
-        Users: make([]*User, 0, 1),
-    }
+// var gPubSubConn *redis.PubSubConn
+//     gPubSubConn = &redis.PubSubConn{Conn: conn}
+
+func generateULID() string {
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	u, _ := ulid.New(ulid.Timestamp(t), entropy)
+	return u.String()
+	// Output: 0000XSNJG0MQJHBF4QX1EFD6Y3
 }
-*/
+
 
 var ws_upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+
+var client_list = make(map[*WS_Client]int64)
 
 
 func main() {
@@ -86,10 +180,9 @@ func main() {
 		panic(err50)
 	}
 
-	//cli_args := os.Args[1:]
-	//fmt.Println(cli_args)
 
-	redis := flag.String("redis", "127.0.0.1:6357", "Redis Server host:port:db")
+	// Read Options
+	redisConn := flag.String("redis", "127.0.0.1:6379", "Redis Server host:port:db")
 	tcpListen := flag.String("listen", ":8080", "Listen Address for TCP")
 	tlsListen := flag.String("listen-tls", ":8443", "Listen Address for TLS/SSL")
 	tlsPemFile := flag.String("tls-pem-file", "server.pem", "Stuff")
@@ -100,17 +193,15 @@ func main() {
 	flag.Parse()
 
 
-	fmt.Println("Connecting to Redis:", *redis)
+	fmt.Println("Connecting to Redis:", *redisConn)
 	fmt.Println("TCP:", *tcpListen)
 	fmt.Println("TLS:", *tlsListen)
 	//fmt.Println("CRT:", *tlsFile)
 	fmt.Println("Origin:", *origin)
 
-	// Fork to Background
-
-
 	// Static Files
 	http.Handle("/", http.FileServer(http.Dir(dir)))
+
 
 	// HTTP Handler
 	http.HandleFunc(*path, func(w http.ResponseWriter, r *http.Request) {
@@ -127,25 +218,53 @@ func main() {
 			fmt.Println(err)
 			return
 		}
+		// defer ws.Close()
 
 		cid := generateULID()
-		client := &WS_Client{ id: cid, ws: ws, pump: make(chan []byte, 256) }
+		client := &WS_Client{ id: cid, ws: ws }
 
-		fmt.Println("New Client:", cid)
+		client_list[ client ] = time.Now().Unix()
+
 		// client.hub.register <- client
-		fmt.Println(client)
+		// fmt.Println(client)
+
+		// Connect to Redis
+		rdbc := redis.NewClient(&redis.Options{
+			Addr: *redisConn,
+			Password: "", // no password set
+			DB: 0,  // use default DB
+		})
+
+		client.pub = rdbc
+
+		client.sub = rdbc.Subscribe("gwsrps")
+		_, err142 := client.sub.Receive()
+		if (err142 != nil) {
+			panic(err142)
+		}
 
 		// Allow collection of memory referenced by the caller by doing all work in
 		// new goroutines.
-		// go client.readPump()
-		// go client.sendPump()
+
+		// Read the PubSub and Forward to Pump
+		go client.incomingPump()
+		go client.outgoingPump()
+
+		go func() {
+			for {
+				var msg, _ = client.sub.ReceiveMessage()
+				fmt.Println("incoming-ps:", msg)
+				client.pump <- []byte(msg.Payload)
+			}
+		}()
+
+		fmt.Println("New Client:", cid, "Clients:", len(client_list))
 
 	})
 
 	// Start Servers in a WaitGroup
 
 	swg := &sync.WaitGroup{}
-
 
 	// TCP Listener
 	swg.Add(1)
@@ -166,6 +285,9 @@ func main() {
 		}
 		swg.Done()
 	}()
+
+	// Fork to Background
+
 
 	swg.Wait()
 
